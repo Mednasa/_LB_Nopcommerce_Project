1cb7c37bca6a7e9c13d62367d12470fc4ad0bf12
Nigar Ayla Ozcanan <158833043+Cognoscentes@users.noreply.github.com>
2024-03-30 21:05:14 +0300

    Added a test case for selecting physical gift cards

--- src/US_506/GiftOrderingProcess_Negative.java
+++ src/US_506/GiftOrderingProcess_Negative.java
@@ -1,9 +1,13 @@
 package US_506;
 
 import Utility.BaseDriver;
+import Utility.Tools;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 // CREDENTIALS
 // Name       : Bradie
@@ -29,5 +33,20 @@ public class GiftOrderingProcess_Negative extends BaseDriver {
         wait.until(ExpectedConditions.visibilityOfAllElements(nop.tabMenu));
         Assert.assertFalse(nop.tabMenu.isEmpty(), "Tab menu is not visible on the homepage!");
 
+    }
+    @Test(priority = 3, groups = {"UI Test"})
+    public void physicalGiftCardSelection(){
+
+        // Add a random "Physical Gift Card" to the cart
+        nop.myClick(nop.giftCardsButton);
+        if (nop.addToCartButtons.size()>=3){
+
+            List<WebElement> lastTwoElement=nop.addToCartButtons.subList(1,nop.addToCartButtons.size());
+            int range= Tools.randomGenerator(2);
+            WebElement randomSelect=lastTwoElement.get(range);
+
+            wait.until(ExpectedConditions.elementToBeClickable(randomSelect));
+            nop.myClick(randomSelect);
+        }
     }
 }

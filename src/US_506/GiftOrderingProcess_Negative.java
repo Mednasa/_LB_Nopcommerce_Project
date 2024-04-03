a130ce803bbac576163c8ad4e59f4684c1de7c86
Nigar Ayla Ozcanan <158833043+Cognoscentes@users.noreply.github.com>
2024-04-03 15:52:02 +0300

    Added DataProvider for negative test cases

--- src/US_506/GiftOrderingProcess_Negative.java
+++ src/US_506/GiftOrderingProcess_Negative.java
@@ -4,16 +4,18 @@ import Utility.BaseDriver;
 import Utility.Tools;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
-// CREDENTIALS
-// Name       : Bradie
-// Lastname   : Joyce
-// Email      : tanna_stylesiysz@vacations.hj
-// Password   : Not4You2Know
-// Website    : https://demo.nopcommerce.com/
+   // CREDENTIALS
+   // Name       : Bradie
+   // Lastname   : Joyce
+   // Email      : tanna_stylesiysz@vacations.hj
+   // Password   : Not4You2Know
+   // Website    : https://demo.nopcommerce.com/
 public class GiftOrderingProcess_Negative extends BaseDriver {
 
     String email = "tanna_stylesiysz@vacations.hj";
@@ -27,6 +29,7 @@ public class GiftOrderingProcess_Negative extends BaseDriver {
         nop.mySendKeys(nop.passwordPlc, password);
         nop.myClick(nop.loginButton);
     }
+
     @Test(priority = 2, dataProvider = "inputs", groups = {"UI Test"})
     public void leaveTheMandatoryFieldsEmpty(String recipientName, String senderName, String message) {
 
@@ -46,5 +49,30 @@ public class GiftOrderingProcess_Negative extends BaseDriver {
             nop.myClick(randomSelect);
 
         }
+        nop.mySendKeys(nop.recipientNamePlc, recipientName);
+        nop.mySendKeys(nop.senderNamePlc, senderName);
+        nop.mySendKeys(nop.messageBox, message);
+
+        wait.until(ExpectedConditions.visibilityOf(nop.addToCartButton));
+        nop.scrollToElement(nop.addToCartButton);
+        Tools.JSClick(nop.addToCartButton);
+
+        wait.until(ExpectedConditions.elementToBeClickable(nop.warningCloseBtn));
+        nop.myClick(nop.warningCloseBtn);
+
+        wait.until(ExpectedConditions.visibilityOf(nop.warningMsg));
+        Assert.assertTrue(nop.warningMsg.isDisplayed(), "No warning message is present!");
+        logger.info("warning message: " + nop.warningMsg.getText());
+
+    }
+    @DataProvider
+    public Object[][] inputs() {
+        Object[][] inputList = {
+
+                {" ", " ", "Enjoy your day!"},
+                {"User123", " ", "Wishing you all the best with this gift!"}
+        };
+        return inputList;
     }
 }
+
